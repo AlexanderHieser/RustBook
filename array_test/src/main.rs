142104646e97e29ddf4ad8109bd3ed987e9b2b55
Alexander Hieser <Alexander.Hieser@online.de>
2022-12-08 15:29:52 +0100

    Play arround with arrays

--- array_test/src/main.rs
+++ array_test/src/main.rs
@@ -1,3 +1,24 @@
+use std::{io, process::exit};
 fn main() {
-    println!("Hello, world!");
+
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Current Array: {:?}", a);
+    println!("Please provide an index (starting with 0");
+
+    let mut index = String::new();
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = match index.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please insert valid index number");
+            exit(-1);
+        }
+    };
+    let element = a[index];
+    println!("Element Value: {element}");
 }
