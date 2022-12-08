0ad02fba7817672618addd4058da96d67ce52ba1
Alexander Hieser <Alexander.Hieser@online.de>
2022-12-08 11:31:17 +0100

    Add some comments

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,17 +4,17 @@ use std::io;
 
 fn main() {
     println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // Generate the random number to guess
+    let secret_number = rand::thread_rng().gen_range(1..=100); // between 1 and 100
     println!("The secret number is: {secret_number}");
-    loop {
+    loop { //Loop until user guessed correctly
         println!("Please input your guess:");
 
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
-            .expect("Failed to read line!");
-        let guess: u32 = match guess.trim().parse() {
+            .expect("Failed to read line!"); // read user input
+        let guess: u32 = match guess.trim().parse() { //cast string to number
             Ok(num) => num,
             Err(_) => {
                 println!("Please insert a number!");
@@ -23,7 +23,7 @@ fn main() {
         };
         println!("You guessed: {guess}");
 
-        match guess.cmp(&secret_number) {
+        match guess.cmp(&secret_number) { // compare guessed and secret random number
             Ordering::Less => println!("Too small"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
